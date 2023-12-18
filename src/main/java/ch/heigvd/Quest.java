9df490f3bc0a94a41d539b7da07ab86142781b6e
olinb <olin.bourquin@gmail.com>
2023-12-18 16:28:57 +0100

    add Quest class

---
+++ src/main/java/ch/heigvd/Quest.java
@@ -0,0 +1,41 @@
+package ch.heigvd;
+
+import java.util.UUID;
+
+public class Quest {
+
+    private final String uuid;
+    private final String name;
+    private final String description;
+    private final int reward;
+
+    public Quest(String name, String description, int reward, String uuid) {
+        this.uuid = uuid;
+        this.name = name;
+        this.description = description;
+        this.reward = reward;
+    }
+
+    public Quest(String name, String description, int reward) {
+        uuid = UUID.randomUUID().toString();
+        this.name = name;
+        this.description = description;
+        this.reward = reward;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getReward() {
+        return reward;
+    }
+}
